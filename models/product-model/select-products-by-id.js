d50397fb91eed5df8bcd51b6b90d4ee44b60b3f0
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-03-30 16:28:17 -0300

    refactor: Create products-model folder and move files to it

--- models/product-model/select-products-by-id.js
+++ models/product-model/select-products-by-id.js
@@ -1,9 +1,9 @@
-const connection = require('./connection');
+const connection = require('../connection');
 
 const getProductsById = async (id) => {
   try {
     const [product] = await connection.execute(
-      'SELECT name, quantity FROM StoreManager.products WHERE id = ?', [id],
+      'SELECT id, name, quantity FROM StoreManager.products WHERE id = ?', [id],
     );
     if (product.length === 0) return console.log('Product not found');
     return product[0];
