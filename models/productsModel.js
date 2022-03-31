0de0ea4163fef9fe66e08c94db4e0af09b69a744
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-03-31 18:58:39 -0300

    feat: Create createProduct and checkProduct in model and service for Req 4

--- models/productsModel.js
+++ models/productsModel.js
@@ -20,7 +20,31 @@ const getProductsById = async (id) => {
   }
 };
 
+// chama um product X do database para checagem
+const checkProduct = async (name) => {
+  const [product] = await connection.execute('SELECT * FROM StoreManager.products WHERE name = ?', 
+    [name]);
+  return product[0];
+};
+
+const createProduct = async ({ name, quantity }) => {
+  try {
+    const query = 'INSERT INTO StoreManager.products (name, quantity) VALUES (?,?)';
+    // [{ }] porque vem na inserção -- rever aula para confirmar 100%
+    const [{ insertId }] = await connection.execute(query, [name, quantity]);
+    return {
+      id: insertId,
+      name,
+      quantity,
+    };
+  } catch (error) {
+    console.error(error);    
+  }
+};
+
 module.exports = {
   getProductsById,
   getAllProducts,
+  createProduct,
+  checkProduct,
 };
--- services/productsServices.js
+++ services/productsServices.js
@@ -10,7 +10,23 @@ const getById = async (id) => {
   return product;
 };
 
+const createProduct = async (product) => {
+  try {
+    const { name } = product;
+    // baseado na codigo o Guima
+    const checkProduct = await productsModel.checkProduct(name);
+    if (checkProduct) {
+      return { message: 'Product already exists' };
+    }
+    const newProduct = await productsModel.createProduct(product);
+    return newProduct;    
+  } catch (error) {
+    console.error(error);    
+  }
+};
+
 module.exports = {
   getAll,
   getById,
+  createProduct,
 };
