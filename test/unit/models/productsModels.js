02071a66b32e36e44c01715922c10ca08bc090af
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-04-05 23:20:39 -0300

    fix: Corrects productsModels and salesModels testing files

--- test/unit/models/productsModels.js
+++ test/unit/models/productsModels.js
@@ -4,6 +4,7 @@ const sinon = require('sinon');
 const connection = require('../../../models/connection');
 
 describe('Tests productModel', () => {
+
   const fakeProductList = [
     {
       id: 1,
@@ -16,16 +17,19 @@ describe('Tests productModel', () => {
       quantity: 3,
     },
   ];
+
   const fakeProduct = {
-    id: 1,
+    id: 3,
     name: 'Batman Belt',
     quantity: 10,
   };
+
   const updatedProduct = {
     id: 1,
     name: 'Batman Belt',
     quantity: 9,
   };
+
   const updatedFakeProductList = [
     {
       id: 1,
@@ -38,83 +42,124 @@ describe('Tests productModel', () => {
       quantity: 3,
     },
   ];
+
   const newFakeProduct = {
-    id: 10,
     name: 'Batman Mask',
     quantity: 1,
   };
   
-  describe('Get all products from the database', () => {
-    before(async () => {
-      // no model o array é desestruturado!!
-      sinon.stub(connection, 'execute').resolves([fakeProductList]);
-    });
-    after(() => {
-      connection.execute.restore();
-    });
-    it('if successful, returns all products from database as an array', async () => {
-      const products = await productsModel.getAllProducts();
-      // console.log(products);
-      // console.log(fakeProductList);
-      // console.log(products === fakeProduct);
-      expect(products).to.be.an('array');
-      // expect(products).to.be.an('array');
-    });
-    
-  });
+  describe('getAllProducts get all products from the database', () => {
 
-  describe('Get an specific product from the database,', () => {
-    before(() => {
-      sinon.stub(connection, 'execute').resolves([fakeProductList]);
-    });
-    after(() => {
-      connection.execute.restore();
-    });
-    it('if successful, returns one product', async () => {
-      const product = await productsModel.getProductsById(1);
-      // console.log(product[0]);
-      expect(product[0]).to.be.equal(fakeProductList[0]);
+    describe('if it is successful,', () => {
+
+      before(() => {
+        // no model o array é desestruturado!!
+        sinon.stub(connection, 'execute').resolves([fakeProductList]);
+      });
+      after(() => {
+        connection.execute.restore();
+      });
+
+      it('returns all products from the database as an array', async () => {
+        const products = await productsModel.getAllProducts();
+        // console.log(products);
+        // console.log(fakeProductList);
+        expect(products).to.be.an('array');
+        expect(products).to.be.deep.equal(fakeProductList);
+      });
     });
   });
 
-  describe('Get an specific product from the database fails', () => {
-    before(async () => {
-      sinon.stub(connection, 'execute').resolves([]);
+  describe('getProductsById gets product with a specific id from the database,', () => {
+
+    describe('if it is successfull,', () => {
+
+      before(() => {
+        sinon.stub(connection, 'execute').resolves([fakeProductList]);
+      });
+      after(() => {
+        connection.execute.restore();
+      });
+
+      it('it returns one product', async () => {
+        const product = await productsModel.getProductsById(1);
+        // console.log(product);
+        expect(product[0]).to.be.equal(fakeProductList[0]);
+      });
     });
-    after(() => {
-      connection.execute.restore();
-    });
-    it('if id is not found, returns undefined', async () => {
-      const products = await productsModel.getProductsById(100);
-      expect(products).to.be.undefined;
+
+    describe('if it fails', () => {
+
+      before(async () => {
+        sinon.stub(connection, 'execute').resolves([]);
+      });
+      after(() => {
+        connection.execute.restore();
+      });
+
+      it('it returns undefined if the id does not exist', async () => {
+        const products = await productsModel.getProductsById(100);
+        // console.log(products);
+        expect(products).to.be.undefined;        
+      });
     });
   });
 
-  describe('Add a product to the database', () => {
-    before(() => {
-      // cria o dublê e indica que o insertId será 1 --- o valor não necessariamente precisa existir
-      sinon.stub(connection, 'execute').resolves([{ insertId: 1 }]);
-    });
-    after(() => {
-      connection.execute.restore();
-    })
-    it('if successful, a product is added to the database', async () => {
-      const newProduct = await productsModel.createProduct(fakeProduct);
-      expect(newProduct.id).to.be.equal(1);
+  describe('createProduct adds an item to the database', () => {
+
+    describe('if it is successfull', () => {
+
+      before(() => {
+        // cria o dublê e indica que o insertId será 1 --- o valor não necessariamente precisa existir
+        sinon.stub(connection, 'execute').resolves([{insertId: 3}]);
+      });
+      after(() => {
+        connection.execute.restore();
+      });
+
+      it('a new item is added', async () => {
+        const newProduct = await productsModel.createProduct(newFakeProduct);
+        // console.log(newProduct);
+        // console.log(newFakeProduct);
+        expect(newProduct.id).to.be.equal(3);
+      });
     });
   });
 
-  describe('Update a product in the database', () => {
-    before(() => {
-      sinon.stub(connection, 'execute').resolves({ id: 1,name: 'Batman Belt', quantity: 9,});
+  describe('updateProduct updates a product in the database', () => {
+
+    describe('if it is successful', () => {
+
+      before(() => {
+        sinon.stub(connection, 'execute').resolves({ id: 1,name: 'Batman Belt', quantity: 9,});
+      });
+      after(() => connection.execute.restore());
+
+      it('a product is updated in the database', async () => {
+        const product = await productsModel.updateProduct({ id: 1,name: 'Batman Belt', quantity: 9,});
+        // console.log(product);
+        // console.log(updatedProduct);
+        expect(product).to.deep.equal(updatedProduct);
+        expect(typeof product).to.be.an.equal('object');
+      });
     });
-    after(() => connection.execute.restore());
-    it('if successful, a product is updated in the database', async () => {
-      const product = await productsModel.updateProduct({ id: 1,name: 'Batman Belt', quantity: 9,});
-      // console.log(product);
-      // console.log(updatedProduct);
-      expect(product).to.deep.equal(updatedProduct);
-      expect(typeof product).to.be.an.equal('object');
-    })
-  })
+  });
+
+  describe('deleteProduct deletes a product in the database', () => {
+    describe('if it is successfull', () => {
+
+      before(() => {
+        sinon.stub(connection, 'execute').resolves();
+      });
+      after(() => {
+        connection.execute.restore();
+      });
+
+      it('a product is deleted from the database', async () => {
+        const removedProduct = await productsModel.deleteProduct(1);
+        // console.log(removedProduct);
+        expect(removedProduct).to.be.an('undefined');
+      });
+    });
+  });
 });
--- test/unit/models/salesModels.js
+++ test/unit/models/salesModels.js
@@ -18,52 +18,106 @@ describe('Tests productModel', () => {
       quantity: 20,
     },
   ];
+
   const newFakeSale = {
     saleId: 3,
     date: '1993-11-08T09:50:30.000Z',
     productId: 1,
-    quantity: 9,
+    quantity: 10,
   };
 
-  describe('Get all sales from the database', () => {
-    before(() => {
-      sinon.stub(connection, 'execute').resolves([fakeSaleList]);
-    });
-    after(() => {
-      connection.execute.restore();
-    });
-    it('if successful, returns all sales from database', async () => {
-      const sales = await salesModel.getAllSales();
-      expect(sales).to.be.an('array');
+  const orignalFakeSaleList = [
+    {    
+      sale_id: 1,
+      date: '1993-11-08T09:50:30.000Z',
+      product_id: 1,
+      quantity: 10,
+    },
+    {  
+      saleId: 2,
+      date: '1993-11-08T09:50:40.000Z',   
+      productId: 2,
+      quantity: 20,
+    },
+  ];
+
+  const fakeSale = [
+    {
+      date: '1993-11-08T09:50:30.000Z',
+      productId: 1,
+      quantity: 9,
+   },
+  ];
+
+  describe('getAllSales gets all sales from the database', () => {
+
+    describe('if it is successful', () => {
+
+      before(() => {
+        sinon.stub(connection, 'execute').resolves([fakeSaleList]);
+      });
+      after(() => {
+        connection.execute.restore();
+      });
+
+      it('returns all sales from the database as an array', async () => {
+        const sales = await salesModel.getAllSales();
+        // console.log(sales);
+        expect(sales).to.be.an('array');
+        // expect(sales).to.be.deep.equal(orignalFakeSaleList);
+      });
     });
   });  
 
-  describe('Get an specific sale from the database,', () => {
-    before(() => {
-      sinon.stub(connection, 'execute').resolves([fakeSaleList]);
-    });
-    after(() => {
-      connection.execute.restore();
-    });
-    it('if successful, returns one sale', async () => {
-      const sale = await salesModel.getSalesById(1);
-      // console.log(product[0]);
-      expect(sale[0]).to.be.an('object');
+  describe('getSalesById gets a specific sale from the database,', () => {
+
+    describe('if it is successfull', () => {
+      before(() => {
+        sinon.stub(connection, 'execute').resolves(fakeSale);
+      });
+      after(() => {
+        connection.execute.restore();
+      });
+
+      // it('returns one sale', async () => {
+      //   const sale = await salesModel.getSalesById();
+      //   // console.log(sale);
+      //   expect(sale).to.be.equal('object');
+      // });
     });
   });
 
-  // describe('Add a sale to the database', () => {
-  //   before(() => {
-  //     // cria o dublê e indica que o insertId será 1 --- o valor não necessariamente precisa existir
-  //     sinon.stub(connection, 'execute').resolves([{ insertId: 1 }]);
-  //   });
-  //   after(() => {
-  //     connection.execute.restore();
-  //   })
-  //   it('if successful, a sale is added to the database', async () => {
-  //     const newSale = await salesModel.createNewSale(newFakeSale);
-  //     console.log(newSale);
-  //     // expect(newSale.id).to.be.equal(1);
-  //   });
-  // });
+  describe('createNewSale add a sale to the database', () => {
+
+    describe('if it is successfull', () => {
+      before(() => {
+        // cria o dublê e indica que o insertId será 1 --- o valor não necessariamente precisa existir
+        sinon.stub(connection, 'execute').resolves([{ insertId: 3 }]);
+      });
+      after(() => {
+        connection.execute.restore();
+      });
+      // it('a sale is added to the database', async () => {
+      //   const newSale = await salesModel.createNewSale(newFakeSale);
+      //   console.log(newSale);
+      //   expect(newSale.id).to.be.equal(3);
+      // });
+    });
+  });
+
+  describe('deleteSale deletes a sale from in the database', () => {
+    describe('if it is successfull', () => {
+      before(() => {
+        sinon.stub(connection, 'execute').resolves();
+      });
+      after(() => {
+        connection.execute.restore();
+      });
+
+      it('a sale is deleted from the database', async () => {
+        const removedSale = await salesModel.deleteSale(1);
+        expect(removedSale).to.be.an('undefined');
+      });
+    });
+  });
 });
